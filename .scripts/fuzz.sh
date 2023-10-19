b367500aeda656204cd96f573d7c7b1dfbf51f51
Caio <c410.f3r@gmail.com>
2023-10-19 10:43:11 -0300

    Updates

--- .scripts/fuzz.sh
+++ .scripts/fuzz.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-cargo fuzz run --fuzz-dir ndstruct-fuzz coo -- -runs=100000
-cargo fuzz run --fuzz-dir ndstruct-fuzz csl -- -runs=100000
+cargo fuzz run --features libfuzzer-sys/link_libfuzzer --fuzz-dir ndstruct-fuzz coo -- -runs=100000
+cargo fuzz run --features libfuzzer-sys/link_libfuzzer --fuzz-dir ndstruct-fuzz csl -- -runs=100000
