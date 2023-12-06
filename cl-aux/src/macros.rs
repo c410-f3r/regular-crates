770d45dfab61a90b1c4a1f3f9bc8e5acd27ee813
Caio <c410.f3r@gmail.com>
2023-12-06 08:40:07 -0300

    Reduce enum size

--- cl-aux/src/macros.rs
+++ cl-aux/src/macros.rs
@@ -3,7 +3,7 @@ macro_rules! _check_capacity {
     let capacity_upper_bound = crate::CapacityUpperBound::capacity_upper_bound($elem);
     let length = crate::Length::length($elem);
     if length >= capacity_upper_bound {
-      return Err(crate::Error::InsufficientCapacity(stringify!($elem), capacity_upper_bound));
+      return Err(crate::Error::InsufficientCapacity(capacity_upper_bound));
     }
   }};
 }
@@ -12,19 +12,19 @@ macro_rules! _check_indcs {
   ($elem:expr, $( $idx:expr ),*) => {{
     let length = crate::Length::length($elem);
     if $( $idx >= length || )* false {
-      return Err(crate::Error::OutOfBounds(stringify!($elem), length));
+      return Err(crate::Error::OutOfBounds(length));
     }
   }};
 }
 
 macro_rules! _get {
   ($elem:expr, $idx:expr) => {{
-    $elem.get($idx).ok_or(crate::Error::OutOfBounds(stringify!($elem), $idx))
+    $elem.get($idx).ok_or(crate::Error::OutOfBounds($idx))
   }};
 }
 
 macro_rules! _get_mut {
   ($elem:expr, $idx:expr) => {{
-    $elem.get_mut($idx).ok_or(crate::Error::OutOfBounds(stringify!($elem), $idx))
+    $elem.get_mut($idx).ok_or(crate::Error::OutOfBounds($idx))
   }};
 }
